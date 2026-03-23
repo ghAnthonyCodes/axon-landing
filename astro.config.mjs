6cb6808116347b32cfcb5be146cd57d6707b6c3c
Anthony Meyer <anthonypaulmeyer@gmail.com>
2026-03-23 19:07:51 -0400

    Converted to Astro for blog posting

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+export default defineConfig({
+  site: 'https://runaxon.com',
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
